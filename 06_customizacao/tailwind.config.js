bba20f835b8195cadd476ba9ea2cc7e2f1083358
Alessandro Viana <alessandroviana830@gmail.com>
2025-02-01 12:53:38 -0300

    Conclusão da Customização

---
+++ 06_customizacao/tailwind.config.js
@@ -0,0 +1,23 @@
+const { colors } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "new-blue": "#243C5A",
+        gray: {
+          ...colors.gray,
+          "900": "#999"
+        }
+      }
+    },
+    spacing: {
+      "50": "20rem"
+    }
+  },
+  plugins: [],
+}
