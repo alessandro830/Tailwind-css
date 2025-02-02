15194c48af2fb74c3630924074dba4bb0a1277ba
Alessandro Viana <alessandroviana830@gmail.com>
2025-02-02 11:09:41 -0300

    Configurando o extend

--- 07_clone_spotify/tailwind.config.js
+++ 07_clone_spotify/tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "14": "3.5rem",
+        "125px": "125px"
+      },
+      colors: {
+        "hoverspt": "#18D760",
+        "purple-main": "#2D46B9",
+        "green-main": "#1ED760"
+      },
+      backgroundImage: theme => ({
+        "postify-theme": "url('/img/burst.svg')",
+        "postify-theme-mobile": "url('/img/burst-mobile.svg')"
+      }),
+      backgroundSize: {
+        "175%":"175%",
+        "195%":"195%"
+      },
+      backgroundPosition: {
+        "banner": "46% 4%",
+        "banner-mobile": "top 25% center"
+      },
+      fontSize: {
+        "9xl": "9rem"
+      }
+    },
   },
   plugins: [],
 }
