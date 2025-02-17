0b729207ecabc8d9e1f6f04743f9c85ef883dc10
Alessandro Viana <alessandroviana830@gmail.com>
2025-02-17 20:25:35 -0300

    Estendendo configurações

--- 14_clone_amazon/tailwind.config.js
+++ 14_clone_amazon/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "arial": "Arial",
+      },
+      colors: {
+        "darkblue-500": "#485769",
+        "darkblue-600": "#37475A",
+        "darkblue-700": "#232F3E",
+        "darkblue-900": "#191E26",
+        "darkblue-950": "#131A22",
+        "gold-100": "#FEBD69",
+        "gold-200": "#FAA63A",
+        "darkblue-650": "#E47911",
+        "darkblue-350": "#F0C148",
+      },
+      spacing: {
+        "7": "1.75rem",
+        "75px": "75px",
+        "210px": "210px",
+      }
+    },
   },
   plugins: [],
 }
